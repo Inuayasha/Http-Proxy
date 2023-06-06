3d2da556e1ad96496eebdefd53f9382a38237391
Arya Murray <themagician948@gmail.com>
2023-06-06 00:01:20 -0400

    Added cors

--- api.js
+++ api.js
@@ -1,11 +1,13 @@
 require('dotenv').config({path: __dirname + '/.env'})
 const express = require('express');
+const cors = require('cors')
 const morgan = require('morgan');
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
 
 // Create Express Server
 const app = express();
+app.use(cors())
 
 // Configuration
 const PORT = process.env.PORT || 3000;
@@ -16,21 +18,12 @@ const API_SERVICE_URL = "https://api-v2.soundcloud.com";
 app.use(morgan('dev'));
 
 
-
-
-// Info GET endpoint
+// Test GET endpoint
 app.get('/test', (req, res, next) => {
     res.send('Test endpoint to ensure connectivity');
  });
 
-// // Authorization
-// app.use('', (req, res, next) => {
-//     if (req.headers.authorization) {
-//         next();
-//     } else {
-//         res.sendStatus(403);
-//     }
-//  });
+
 
 // Proxy endpoints
 app.use('/api', createProxyMiddleware({
@@ -40,10 +33,6 @@ app.use('/api', createProxyMiddleware({
         [`^/api`]: '',
     },
  }));
-
-
-
-
 
  // Start the Proxy
 app.listen(PORT, HOST, () => {
